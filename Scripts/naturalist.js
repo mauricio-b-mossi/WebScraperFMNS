e7a2d0ee12930a7b32feb6e70f8d4ed1d912ddb4
mauricio.b.mossi <mauricio.b.mossi@gmail.com>
2023-11-19 20:14:06 -0500

    added advances

--- Scripts/naturalist.js
+++ Scripts/naturalist.js
@@ -1,6 +1,4 @@
 const puppeteer = require("puppeteer")
-const extractUrl = require("./url_utlis.js")
-const scroller = require("./scroller.js")
 const fs = require("node:fs")
 
 // Check for paramters.
@@ -14,9 +12,11 @@ async function main(query) {
     await page.type("input.form-control.ui-autocomplete-input", query)
 
     await page.waitForSelector("ul.ui-autocomplete.ui-front.ui-widget.ui-widget-content.ac-menu.open li.ac-result.ui-menu-item")
+
     await page.$eval("li.ac-result.ui-menu-item", (el) => {
         el.click()
     })
+
     console.log("Clicked")
     await page.waitForNavigation()
     console.log("Navigated")
@@ -30,16 +30,40 @@ async function main(query) {
                 scroll = window.scrollY;
                 window.scrollBy(0, window.innerHeight)
                 await new Promise((res, _) => {
-                    setTimeout(res, 1000)
+                    setTimeout(res, 500)
                 })
             } while (window.scrollY > scroll)
         })()
     })
 
+    await new Promise((res, _) => {
+        setTimeout(res, 8000)
+    }) 
+
+
+    const items = await page.$$eval("a.photo.has-photo", (els) => {
+        return els.map((el) => el.style['background-image'])
+    })
+
     console.log(items)
     console.log(items.length)
+
+    const urls = items.map(it => extractUrl(it, '"'))
+    console.log(urls)
+
+    fs.open("out.csv", "w", (err, fd) => {
+        fs.appendFile(fd, Buffer.from(urls.toString()), (err) => {
+            if(err) throw err;
+            console.log("Finished")
+        })
+    })
+
     console.log("Finished")
 
 }
 
 main("Lion")
+
+function extractUrl(str, delimeter){
+    return str.slice(str.indexOf(delimeter) + delimeter.length, str.lastIndexOf(delimeter))
+}
--- Scripts/url_utlis.js
+++
@@ -1,5 +0,0 @@
-function extractUrl(str, delimeter){
-    return str.slice(str.indexOf(delimeter) + delimeter.length, str.lastIndexOf(delimeter))
-}
-
-module.exports = extractUrl;
